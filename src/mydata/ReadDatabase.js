8316177c4c6ef74d571ae5be209844ab60d9e083
GJ <jagadeesh15508300@gmail.com>
2021-12-21 17:55:19 +0530

    first step of split file for database

--- src/mydata/ReadDatabase.js
+++ src/mydata/ReadDatabase.js
@@ -1,22 +1,20 @@
 import database from '@react-native-firebase/database';
-import {StyleSheet, View, Button} from 'react-native';
 import * as React from 'react';
 import {useState} from 'react';
-let mydata=[];
-function ReadDatabase()
-{
-    const reference = database().ref('/user');
-    database()
+let mydata = [];
+
+export const ReadDatabase = function (data, fromindex1, toindex2) {
+  const reference = database().ref('/user');
+  database()
     .ref('/user')
     .once('value')
     .then(snapshot => {
       console.log('User data read once: ', snapshot.val());
-      mydata=
-      mydata.push(snapshot.val())
-      console.log("mydata",mydata);
-    }) 
-    .catch(err => console.error("error: " + err));
-    console.log("mydata 001",mydata);
-      return ( mydata ); 
-}
-export default ReadDatabase;
+      mydata = [];
+      mydata.push(snapshot.val());
+      console.log('mydata', mydata);
+    })
+    .catch(err => console.error('error: ' + err));
+  console.log('mydata 001', mydata);
+  return mydata;
+};
